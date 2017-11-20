ab54bf96985f1f11d3b7ee04fd9bbd4255508226
Giorgi Fafakerashvili <george.fafakerashvili@azry.com>
2017-11-20 12:58:11 +0400

    first commit

---
+++ cmake-build-debug/CMakeFiles/JsonParser.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JsonParser.dir/main.cpp.obj"
+  "JsonParser.pdb"
+  "JsonParser.exe"
+  "JsonParser.exe.manifest"
+  "libJsonParser.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JsonParser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
